5fed89fed34fa695fba8f36ddb77dfd30deb9e18
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-04-10 10:17:52 +0300

    Add сonfigure collection, section header to PeopleViewController, demo users.json and models MChat, MUser

---
+++ IChat/Models/MUser.swift
@@ -0,0 +1,23 @@
+//
+//  MUser.swift
+//  IChat
+//
+//  Created by Evgeniy Fakhretdinov on 10.04.2024.
+//
+
+import UIKit
+
+struct MUser: Hashable, Decodable {
+    var username: String
+    var avatarStringURL: String
+    var id: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MUser, rhs: MUser) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
